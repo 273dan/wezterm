e68c7a42024e774a95d2084be655360fc4aa550d
Dan Kirby <dankirby273@gmail.com>
2026-03-16 15:53:27 +0000

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- Colours and font
+config.color_scheme = "Gruvbox Dark (Gogh)"
+config.font = wezterm.font "FantasqueSansM Nerd Font"
+
+-- Tab bar
+config.use_fancy_tab_bar = false
+
+
+-- Keybinds
+config.keys = {
+    {
+        key = 'RightArrow',
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        key = 'LeftArrow',
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+}
+
+
+
+return config
