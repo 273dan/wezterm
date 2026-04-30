8512e018e75ad2a568749e4d559e61abf7ee1579
Dan Kirby <dankirby273@gmail.com>
2026-04-30 23:54:45 +0100

    added binds to move tab, added full opacity for sway only

--- wezterm.lua
+++ wezterm.lua
@@ -5,9 +5,13 @@ local config = {}
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font "FantasqueSansM Nerd Font"
 
+-- Transparency
+if os.getenv("XDG_CURRENT_DESKTOP") ~= "sway:wlroots:swayfx" then
+  config.window_background_opacity = 0.7
+end
+
 -- Tab bar
 config.use_fancy_tab_bar = false
-
 
 -- Keybinds
 config.keys = {
@@ -24,10 +28,18 @@ config.keys = {
     {
         key = '{',
         mods = "CTRL|SHIFT",
-        action = wezterm.action.ActivateCopyMode()
+        action = wezterm.action.ActivateCopyMode
+    },
+    {
+        key = 'RightArrow',
+        mods = "CTRL|SHIFT|ALT",
+        action = wezterm.action.MoveTabRelative(1)
+    },
+    {
+        key = 'LeftArrow',
+        mods = "CTRL|SHIFT|ALT",
+        action = wezterm.action.MoveTabRelative(-1)
     },
 }
-
-
 
 return config
